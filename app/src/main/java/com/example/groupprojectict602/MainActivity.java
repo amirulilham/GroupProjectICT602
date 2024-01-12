4502be1f147b817247e1174adf18bee8ad3f8fd4
amirulilham <2022484752@student.uitm.edu.my>
2024-01-13 03:33:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/groupprojectict602/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.groupprojectict602;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton openActivity;
+    //ImageButton openActivity2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openActivity = (ImageButton) findViewById(R.id.imageButton1);
+        openActivity = (ImageButton) findViewById(R.id.imageButton2);
+    }
+
+    public void openActivity1(View view){
+        Intent intent = new Intent (this, Group_Members.class);
+        startActivity(intent);
+    }
+
+    public void openActivity2(View view){
+        Intent intent = new Intent (this, SearchLocations.class);
+        startActivity(intent);
+    }
+}
